aa70e39f27c274c90ee55c771b59229a31d57f1c
willry <wiribasczky@gmail.com>
2024-09-11 10:44:31 -0300

    Otimizações de coleta de prints

--- publisher.js
+++ publisher.js
@@ -11,13 +11,17 @@ class Publisher {
       
     }
 
+    this.chunkSize = 3;
 
-    this.chunkSize = 1;
     this.chunks = [];
 
     this.workers = [];
 
     this.configurarGracefulShutdown();
+
+    const randomInt = Math.floor(Math.random() * 1000);
+    
+    this.identificadorColetaAtual = randomInt;
   }
 
   configurarGracefulShutdown() {
@@ -58,12 +62,6 @@ class Publisher {
       });
 
       await Promise.all(promises);
-
-      // await new Promise((resolve, reject) => {
-      //   setTimeout(() => {
-      //     resolve();
-      //   }, 2500)
-      // });
     }
 
     await this.browser.close();
@@ -72,8 +70,10 @@ class Publisher {
   async startBrowser() {
     this.browser = await puppeteer.launch({
       args: [
+        `--port=${this.identificadorColetaAtual}`,
         '--no-sandbox',
         '--disable-setuid-sandbox',
+        '--disable-logging',
         '--disable-dev-shm-usage',
         '--disable-session-crashed-bubble',
         '--disable-accelerated-2d-canvas',
@@ -81,15 +81,20 @@ class Publisher {
         '--no-zygote',
         '--single-process',
         '--noerrdialogs',
-        '--disable-gpu'
+        '--disable-gpu',
+        '--disable-cache',
+        '--disk-cache-size=0',
+        '--disable-application-cache',
+        '--silent',
       ],
     });
   }
 
   async ssr(url, nomePrint = "print.png") {
+    let page = null;
     try {
       console.log(`Coleta iniciada: ${url}`);
-      const page = await this.browser.newPage();
+      let page = await this.browser.newPage();
       await page.setViewport({ width: 1920, height: 1080 });
 
       await page.goto(url, { waitUntil: "networkidle0" });
@@ -98,21 +103,30 @@ class Publisher {
       console.log(`Coleta realizada: ${url}`);
     } catch (err) {
       throw new Error(`Exception: ${err.message}`);
+    } finally {
+      if(page) {
+        await page.close();
+      }
     }
   }
 
-  gracefulShutdown() {
-    return Promise.all(
-      this.workers.map((worker) => {
-        return new Promise((resolve) => {
-          worker.postMessage("shutdown");
-          worker.once("exit", (code) => {
-            console.log(`Worker exited with code`);
-            resolve();
-          });
-        });
-      })
-    );
+  matarProcessosPorPametroDeIdentificador() {
+    const identificadorParaMatar = `--port=${this.identificadorColetaAtual}`;
+
+    const exec = require("child_process").exec;
+    
+    exec(`ps aux | grep "${identificadorParaMatar}" | awk '{print $2}' | xargs kill -9`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      console.error(`stderr: ${stderr}`);
+    });
+  }
+
+  async gracefulShutdown() {
+    this.matarProcessosPorPametroDeIdentificador();
   }
 }
 
